11603fb884e613aaf53b46ac6610bcbe194e7bce
Jesus <je.flores28@gmail.com>
2022-11-10 22:58:15 -0600

    front end complete

---
+++ components/Footer.js
@@ -0,0 +1,34 @@
+function Footer() {
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600">
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">ABOUT</h5>
+        <p>New Airbnb works</p>
+        <p>Newsroom</p>
+        <p>Investors</p>
+        <p>Airbnb Plus</p>
+        <p>Airbnb Luxe</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">COMMUNITY</h5>
+        <p>Accessibility</p>
+        <p>This is not a real site</p>
+        <p>It's a pretty awesome clone</p>
+        <p>Welcome and Enjoy</p>
+        <p> - Jesus</p>
+      </div>
+
+      <div className="space-y-4 text-xs text-gray-800">
+        <h5 className="font-bold">HOST</h5>
+        <p>Clones are fun</p>
+        <p>And GREAT ideas for projects</p>
+        <p>Recreate what's already been created</p>
+        <p>And as alway</p>
+        <p>Keep on HACKING!</p>
+      </div>
+    </div>
+  );
+}
+
+export default Footer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,30 +1,71 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
 
-const Home: NextPage = () => {
+import Header from "../components/Header";
+import Banner from "../components/Banner";
+import SmallCard from "../components/SmallCard";
+import MediumCard from "../components/MediumCard"
+import LargeCard from "../components/LargeCard"
+import Footer from "../components/Footer"
+
+const Home: NextPage = ({ exploreData, cardsData }) => {
+
   return (
-    <div className="flex min-h-screen flex-col items-center justify-center py-2">
+    <div className="">
       <Head>
         <title>Airbnb-Clone</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-     
+      <Header />
+      <Banner />
 
-      <footer className="flex h-24 w-full items-center justify-center border-t">
-        <a
-          className="flex items-center justify-center gap-2"
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-        </a>
-      </footer>
+      <main className="max-w-7xl m-auto px-8 sm:px-16">
+
+        <section className="pt-6">
+          <h2 className="text-4xl font-semibold pb-5">Explore Nearby</h2>
+          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+            {exploreData.map(({ img, location, distance }) => (
+              <SmallCard img={img} location={location} distance={distance} />
+            ))}
+          </div>
+        </section>
+
+        <section>
+          <h2 className="text-4xl font-semibold py-8">Live Anywhere</h2>
+              <div className="flex space-x-3 overflow-scroll scrollbar-hide p-3 ml-3">
+                {cardsData.map(({img, title}) => <MediumCard img={img} title={title} />)}
+              </div>
+        </section>
+
+        <LargeCard
+        img="https://links.papareact.com/4cj"
+        title="The Greatest Outdoors"
+        description="Wishlist curated by Airbnb."
+        buttonText="Get inspired"
+        />
+      </main>
+
+      <Footer/>
     </div>
-  )
+  );
+};
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https:///www.jsonkeeper.com/b/4G1G").then(
+    (res) => res.json()
+  );
+
+  const cardsData = await fetch("https://www.jsonkeeper.com/b/VHHT").then(res => res.json())
+
+
+  return {
+    props: {
+      exploreData,
+      cardsData
+    },
+  };
 }
 
-export default Home
+export default Home;
